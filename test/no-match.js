7625a6ab3fd508d106d46e1239ebafec00c1fb91
sboussaid <sboussaid3@gmail.com>
2016-01-11 19:33:48 +0100

    Adds dummy test

--- test/no-match.js
+++ test/no-match.js
@@ -4,7 +4,7 @@ import NoMatch from '../src/no-match.js';
 describe('NoMatch view aka 404 page', () => {
 
   it('should have props.children equal to "No Match.".', () => {
-    assert.equal(NoMatch().props.children, 'No Match. No Match.');
+    assert.equal(NoMatch().props.children, 'No Match.');
   });
 
 });
