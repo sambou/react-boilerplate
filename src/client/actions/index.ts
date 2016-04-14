2e624fc197bafa68d661819696058d4c7284114f
sboussaid <sboussaid3@gmail.com>
2016-04-14 08:44:33 +0200

    Add support for async/await

--- src/client/actions/index.ts
+++ src/client/actions/index.ts
@@ -10,31 +10,32 @@ interface GitHubAPIResponse {
 
 type AsyncAction<T> = (dispatch: (a: Action) => void, getState: () => any) => Promise<T>;
 
-export function changeAsyncSampleText(text): AsyncAction<GitHubAPIResponse> {
-  return (dispatch, getState) => {
-
+export function changeAsyncSampleText(text): AsyncAction<string> {
+  return async function(dispatch, getState) {
     dispatch(changeSampleText('LOADING...'));
+    let response = await getUsers();
 
-    let p = fetch(`https://api.github.com/users?since=${Math.floor(Math.random() * 1000)}`);
+    try {
+      let users = await parseUsers(response);
+      dispatch(changeSampleText(users[0].login));
 
-    return p
-      .then(
-        (res): Promise<GitHubAPIResponse> => {
-          if (res.ok) {
-            return res.json();
-          } else {
-            throw new Error('Something went wrong.');
-          }
-        }
-      )
-      .then(
-        users => {
-          dispatch(changeSampleText(users[0].login));
-          return users;
-        } ,
-        e => dispatch(changeSampleText(e.toString()))
-      );
+      return users;
+    } catch(e) {
+      dispatch(changeSampleText(e.toString()))
+    }
   };
+}
+
+async function getUsers() {
+  return fetch(`https://api.github.com/users?since=${Math.floor(Math.random() * 1000)}`);
+}
+
+async function parseUsers(response) {
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error('Something went wrong.');
+  }
 }
 
 export function changeSampleText(text) {
